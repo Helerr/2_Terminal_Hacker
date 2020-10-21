fc848e0f16929915df7a301aba3a950fcf25e541
Neculai Alexandru <31351317+Helerr@users.noreply.github.com>
2020-10-21 19:50:36 +0300

    ShowMainMenu() fix
    removed argument from ShowMainMenu(), fixed the error

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -8,7 +8,7 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        ShowMainMenu("Ted");
+        ShowMainMenu();
     }
 
     void ShowMainMenu()
