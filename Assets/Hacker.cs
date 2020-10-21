1abb7cdd2d41da859baa870a7264fbf69e85abd9
Neculai Alexandru <31351317+Helerr@users.noreply.github.com>
2020-10-21 16:09:13 +0300

    ShowMainMenu()
    Implemented function to show the main menu output

---
+++ Assets/Hacker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {    
+        Terminal.ClearScreen();
+        Terminal.WriteLine("We need someone to hack into various facilities. Can you do it?");
+        Terminal.WriteLine("Press 1 for the Daily Life Corp.");
+        Terminal.WriteLine("Press 2 for the AMD Computers");
+        Terminal.WriteLine("Press 3 for the All Knowing Organization");
+        Terminal.WriteLine("Do you need a backstory? Press 4");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
