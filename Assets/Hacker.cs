04c94da9622c3ba3517ee4fa7ada5898fda61fdb
Neculai Alexandru <31351317+Helerr@users.noreply.github.com>
2020-10-21 19:36:14 +0300

    fix postponed for dense EE
    commented out Easter Egg for Dense player, will fix later

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
@@ -8,13 +9,11 @@ public class Hacker : MonoBehaviour
     void Start()
     {   
         ShowMainMenu("Ted");
-        
     }
 
-    void ShowMainMenu(string userName)
+    void ShowMainMenu()
     {    
         Terminal.ClearScreen();
-        Terminal.WriteLine("Hello there, " + userName);
         Terminal.WriteLine("We need someone to hack into various facilities. Can you do it?");
         Terminal.WriteLine("Press 1 for the Daily Life Corp.");
         Terminal.WriteLine("Press 2 for the AMD Computers");
@@ -24,8 +23,9 @@ public class Hacker : MonoBehaviour
     }
     void OnUserInput(string input)
     {
+        var count = 0;
         if ( input == "007") {
-            Terminal.WriteLine("Please choose your level Mr. Bond");
+            Terminal.WriteLine("Please choose your level Mr. Bond!");
         }
         else if (input == "420") {
             Terminal.WriteLine("Blaze it! No need to rush, choose your level when you're done.");
@@ -35,10 +35,17 @@ public class Hacker : MonoBehaviour
             Terminal.WriteLine("You chose level 1");
         } else if (input == "menu")
         {
-            ShowMainMenu("Ted");
+            ShowMainMenu();
         }
         else {
-            Terminal.WriteLine("False");
+            Terminal.WriteLine("Please choose a valid level.");
+            count+=1;
+            /*if (count == 3)
+            {
+                ShowMainMenu();
+                Terminal.WriteLine("You must be dense. Please read the main menu again.CAREFULLY!");
+                Terminal.WriteLine("CHOOSE FROM THE LEVELS LISTED ABOVE!");
+            }*/
         }
     }
     // Update is called once per frame
